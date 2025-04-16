6200c5666980ea6789f448361b4b5cee5c506276
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-04-16 15:58:20 -0500

    feat: reset password form

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,10 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form"
+
+
+export const NewPasswordPage  = () => {
+  return (
+    <NewPasswordForm />
+  )
+} 
+
+export default NewPasswordPage
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,12 @@
 import * as z from "zod";
+
+export const NewPasswordSchema = z.object({
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters long"
+    }),
+})
+
+
 export const ResetSchema = z.object({
     email: z.string().email({
         message: "Please enter a valid email address"
@@ -29,3 +37,4 @@ export const RegisterSchema = z.object({
 export type RegisterSchema = z.infer<typeof RegisterSchema>
 export type LoginSchema = z.infer<typeof LoginSchema>
 export type ResetSchema = z.infer<typeof ResetSchema>
+export type NewPasswordSchema = z.infer<typeof NewPasswordSchema>
